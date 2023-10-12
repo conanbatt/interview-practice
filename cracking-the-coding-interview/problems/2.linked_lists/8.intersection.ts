da720ab45885c004175c0cf0c4bfde5cbdc7429f
Delca <mati.dc89@gmail.com>
2023-10-13 00:08:18 +0200

    add intersection resolution

---
+++ cracking-the-coding-interview/problems/2.linked_lists/8.intersection.ts
@@ -0,0 +1,51 @@
+// 8.  *Intersection*; Given two (singly) linked lists, determine if the two lists intersect.
+// Return the inter- secting node. Note that the intersection is defined based on reference, not value.
+// That is, if the kth node of the first linked list is the exact same node (by reference) 
+// as the j t h node of the second linked list, then they are intersecting.
+
+
+class LinkedNode {
+  value;
+  next;
+  constructor(value) {
+    this.value = value;
+    this.next = null; // Initialize 'next' as null
+  }
+}
+
+const a = new LinkedNode('a');
+const b = new LinkedNode('b');
+const c = new LinkedNode('c');
+const d = new LinkedNode('d');
+const e = new LinkedNode('e');
+const f = new LinkedNode('f');
+
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+const g = new LinkedNode('g');
+g.next = d;
+
+
+const getIntersection = (nodeA, nodeB) => {
+  const nodeSet = new Set()
+
+  let actualNodeA = nodeA
+  while(actualNodeA) {
+    nodeSet.add(actualNodeA)
+    actualNodeA = actualNodeA.next
+  }
+
+  let actualNodeB = nodeB
+  while (actualNodeB) {
+    const hasIntersection = nodeSet.has(actualNodeB)
+    if(hasIntersection) return actualNodeB
+    actualNodeB = actualNodeB.next
+  }
+}
+
+getIntersection(a, g)
