ac58ca33409c27312afb3ebf26d8317d0c7e16ff
Delca <mati.dc89@gmail.com>
2023-10-21 10:58:10 +0200

    fix checkPalindrome

--- cracking-the-coding-interview/problems/2.linked_lists/7.palindrome.ts
+++ cracking-the-coding-interview/problems/2.linked_lists/7.palindrome.ts
@@ -1,40 +1,91 @@
 // 7. *Palindrome*: Implement a function to check if a linked list is a palindrome.
-
+// with no extra structure
 
 class LinkedNode {
   value;
   next;
   constructor(value) {
     this.value = value;
-    this.next = null; // Initialize 'next' as null
+    this.next = null;
   }
 }
 
 const a = new LinkedNode('a');
 const b = new LinkedNode('b');
 const c = new LinkedNode('c');
-const d = new LinkedNode('c');
-const e = new LinkedNode('b');
-const f = new LinkedNode('a');
+const x = new LinkedNode('x');
+const d = new LinkedNode('1');
+const e = new LinkedNode('2');
+const f = new LinkedNode('3');
 
 a.next = b;
 b.next = c;
-c.next = d;
+c.next = x;
+x.next = d;
 d.next = e;
 e.next = f;
 
-const checkPalindrome = (node) => {
-  const arr = []
+const a1 = new LinkedNode('a');
+const b1 = new LinkedNode('b');
+const c1 = new LinkedNode('c');
+const x1 = new LinkedNode('x');
+const d1 = new LinkedNode('c');
+const e1 = new LinkedNode('b');
+const f1 = new LinkedNode('a');
 
-  let actualNode = node
-  while(actualNode) {
-    arr.push(actualNode.value)
-    actualNode = actualNode.next
+a1.next = b1;
+b1.next = c1;
+c1.next = x1;
+x1.next = d1;
+d1.next = e1;
+e1.next = f1;
+
+const a2 = new LinkedNode('a');
+const b2 = new LinkedNode('b');
+const c2 = new LinkedNode('c');
+const d2 = new LinkedNode('c');
+const e2 = new LinkedNode('b');
+const f2 = new LinkedNode('a');
+
+a2.next = b2;
+b2.next = c2;
+c2.next = d2;
+d2.next = e2;
+e2.next = f2;
+
+const checkPalindrome = (node) => {
+  if (!node || !node.next) return true;
+
+  let slow = node
+  let fast = node?.next
+  while (fast) {
+    slow = slow.next
+    fast = fast.next?.next
   }
 
-  const arrjoined = arr.join('')
+  let current = slow
+  let prev = null;
+  while (current) {
+    const nextNode = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextNode;
+  }
 
-  return arr.reverse().join('') === arrjoined
+  let firstHalf = node;
+  let secondHalf = prev;
+  while (secondHalf) {
+    if (firstHalf.value !== secondHalf.value) {
+      return false;
+    }
+    firstHalf = firstHalf.next;
+    secondHalf = secondHalf.next;
+  }
+  return true;
 }
 
-checkPalindrome(a)
+console.log('check a: ',checkPalindrome(a))
+console.log('----------------------------------------------------------------')
+console.log('check a1: ',checkPalindrome(a1))
+console.log('----------------------------------------------------------------')
+console.log('check a2: ',checkPalindrome(a2))
