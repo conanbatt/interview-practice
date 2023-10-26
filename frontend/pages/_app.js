67aae941bec6424d358b90ff9a67d2023d885e4c
Delca <mati.dc89@gmail.com>
2023-10-26 08:25:59 +0200

    add market solution

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import './challenges/signup_solution.css'
 import './challenges/hooks_solution.css'
 import './challenges/file-tree-viewer_solution.css'
+import './challenges/market_solution.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
