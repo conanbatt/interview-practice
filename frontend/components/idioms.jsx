54840b1bf8709a0a9bc3fe9e5f2b34474ecb8d95
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-02-16 14:50:35 -0300

    idioms

--- frontend/components/idioms.jsx
+++ frontend/components/idioms.jsx
@@ -17,14 +17,20 @@ export function FunctionsAsComponents({ buttonText = 'Start Now' }) {
   </div>)
 }
 
-export function ObjectCopying(object) {
+export function objectCopying() {
   const object = { a: { c: 1 }, b: 2 }
   const copy = { ...object }
   copy.a.c = 2
   return { ...object }
 }
 
-export function useEffect({ fetchURL, label }) {
+export function arrayCopying() {
+  const array = [{ a: 1}, {a: 2}, {a: 3}]
+  const copy = [...array]
+  return copy;
+}
+
+export function UseEffect({ fetchURL, label }) {
   useEffect(async () => {
     await fetch(fetchURL)
   })
@@ -36,7 +42,7 @@ export function useEffect({ fetchURL, label }) {
   )
 }
 
-export function useEffectDerivedCalculation(object) {
+export function UseEffectDerivedCalculation(object) {
   const [sum, setSum] = useState()
   const [clickedTimes, setClickedTimes] = useState()
 
@@ -56,7 +62,7 @@ export function useEffectDerivedCalculation(object) {
   )
 }
 
-export function useEffectLifeCycle(object) {
+export function UseEffectLifeCycle(object) {
   const [loaded, setLoaded] = useState()
 
   useEffect(() => {
@@ -75,7 +81,7 @@ export function useEffectLifeCycle(object) {
   )
 }
 
-export function avoidingUseState(object) {
+export function AvoidingUseState(object) {
   const ref = useRef('Unmounted');
 
   useEffect(() => {
@@ -89,7 +95,7 @@ export function avoidingUseState(object) {
   )
 }
 
-export function avoidingUseState(object) {
+export function AvoidingUseState(object) {
   const ref = useRef('Unmounted');
 
   useEffect(() => {
@@ -101,4 +107,87 @@ export function avoidingUseState(object) {
       {ref.current}
     </div>
   )
+}
+
+
+async function API() { return true }
+
+export function UntraceableState(object) {
+  const [result, setResult] = useState()  
+
+  useEffect(async () => {
+    let loading = true;
+    const result = await API();
+    loading = false;
+    setResult(result)
+  }, [])
+
+  return (
+    <div>
+      Result:{result}
+    </div>
+  )
+}
+
+export function CrudeDeclarations() {
+  const calendarDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  return (<ol>
+    {calendarDays.map((val) => <span key={val}>{val}</span>)}
+  </ol>)
+}
+
+export function MagicNumbers(age) {
+  return (<ol>
+    { age < 18 ? <div>Spicy</div> : <div>You are not old enough</div>}
+  </ol>)
+}
+
+export function unidiomaticHTMLStructure() {
+  const [name, setName] = useState("")
+  const handleSubmit = (e) => {}
+
+  return (<div>
+    <input value={name} name="name" type="text" onChange={setName} />
+    <button type="submit" onClick={handleSubmit}>Submit</button>
+  </div>)
+}
+
+export function CrudeStateManagement() {
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [location, setLocation] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+
+  const handleSubmit = (e) => {}
+
+  return (<form onSubmit={handleSubmit}>
+    <input value={name} name="name" type="text" onChange={setName} />
+    <input value={age} name="age" type="number" onChange={setAge} />
+    <input value={location} name="location" type="text" onChange={setLocation} />
+    <input value={email} name="email" type="email" onChange={setEmail} />
+    <input value={password} name="password" type="password" onChange={setPassword} />
+    <button type="submit">Submit</button>
+  </form>)
+}
+
+export function UnidiomaticHTMLHierarchy() {
+  const bids = [1,2,3]
+  const asks = [1,2,3]
+
+  return (<li>
+    {bids.map((bid, i) => <span key={i}>{bid}</span>)}
+    {asks.map((ask, j) => <span key={j+'asks'}>{ask}</span>)}
+  </li>)
+}
+
+export function UnidiomaticHTMLHierarchy() {
+  const bids = [1,2,3]
+  const asks = [1,2,3]
+
+  return (<li>
+    {bids.map((bid, i) => <span key={i}>{bid}</span>)}
+    {asks.map((ask, j) => <span key={j+'asks'}>{ask}</span>)}
+  </li>)
 }
