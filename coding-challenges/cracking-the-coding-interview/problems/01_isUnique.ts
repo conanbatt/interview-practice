49e8fa3992604ad64201cfa1965666692623c06d
mdecarlo <mati.dc89@gmail.com>
2024-05-15 17:18:45 -0300

    init coding challenges project

---
+++ coding-challenges/cracking-the-coding-interview/problems/01_isUnique.ts
@@ -0,0 +1,5 @@
+// 1. *Is Unique*: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+export default function isUnique (str : string): boolean {
+
+}
