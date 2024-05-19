dfffc524fcc927baf8f68787128c28fe8cb36db3
mdecarlo <mati.dc89@gmail.com>
2024-05-19 15:31:22 -0300

    fix loopNode test

--- coding-challenges/cracking-the-coding-interview/problems/__tests__/19_loopDetection.test.ts
+++ coding-challenges/cracking-the-coding-interview/problems/__tests__/19_loopDetection.test.ts
@@ -15,20 +15,20 @@ describe('loopDetection', () => {
     });
 
     test('returns the node at the beginning of the loop', () => {
-        // List with a loop: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 3
-        const loopNode: Node<number> = { value: 3 };
+        // List with a loop: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 31 -> 32 -> 31
+        const loopNode: Node<number> = { value: 31, next: { value: 32 } };
+        loopNode.next!.next = loopNode
         const list: Node<number> = { value: 1, next: { value: 2, next: { value: 3, next: { value: 4, next: { value: 5, next: { value: 6, next: { value: 7, next: { value: 8, next: { value: 9, next: loopNode } } } } } } } } };
-        loopNode.next = list.next!.next;
 
         const result = loopDetection(list);
         expect(result).toEqual(loopNode);
     });
 
     test('returns the node at the beginning of the loop (longer loop)', () => {
-        // List with a loop: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 6
-        const loopNode: Node<number> = { value: 6 };
+        // List with a loop: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 11 -> 12 -> 13 -> 11
+        const loopNode: Node<number> = { value: 11, next: {value: 12, next: {value: 13}}};
+        loopNode.next!.next!.next = loopNode
         const list: Node<number> = { value: 1, next: { value: 2, next: { value: 3, next: { value: 4, next: { value: 5, next: { value: 6, next: { value: 7, next: { value: 8, next: { value: 9, next: { value: 10, next: loopNode } } } } } } } } } };
-        loopNode.next = list.next!.next!.next!.next!.next!.next!.next!.next!.next;
 
         const result = loopDetection(list);
         expect(result).toEqual(loopNode);
