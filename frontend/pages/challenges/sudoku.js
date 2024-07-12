c2014aa9322ec1af35a2228ffba6ca9cf15b1a29
Jenaro Calviño <jen.calvineo@gmail.com>
2024-07-12 16:09:58 -0300

    fix: improve on the subgrid checking

--- frontend/pages/challenges/sudoku.js
+++ frontend/pages/challenges/sudoku.js
@@ -8,107 +8,6 @@ import { useState } from "react";
 class Sudoku {
   constructor() {
     this.board = new Array(9).fill(0).map(() => new Array(9).fill(undefined));
-    this.subgrids = [
-      [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [1, 0],
-        [1, 1],
-        [1, 2],
-        [2, 0],
-        [2, 1],
-        [2, 2],
-      ],
-      [
-        [0, 3],
-        [0, 4],
-        [0, 5],
-        [1, 3],
-        [1, 4],
-        [1, 5],
-        [2, 3],
-        [2, 4],
-        [2, 5],
-      ],
-      [
-        [0, 6],
-        [0, 7],
-        [0, 8],
-        [1, 6],
-        [1, 7],
-        [1, 8],
-        [2, 6],
-        [2, 7],
-        [2, 8],
-      ],
-      [
-        [3, 0],
-        [3, 1],
-        [3, 2],
-        [4, 0],
-        [4, 1],
-        [4, 2],
-        [5, 0],
-        [5, 1],
-        [5, 2],
-      ],
-      [
-        [3, 3],
-        [3, 4],
-        [3, 5],
-        [4, 3],
-        [4, 4],
-        [4, 5],
-        [5, 3],
-        [5, 4],
-        [5, 5],
-      ],
-      [
-        [3, 6],
-        [3, 7],
-        [3, 8],
-        [4, 6],
-        [4, 7],
-        [4, 8],
-        [5, 6],
-        [5, 7],
-        [5, 8],
-      ],
-      [
-        [6, 0],
-        [6, 1],
-        [6, 2],
-        [7, 0],
-        [7, 1],
-        [7, 2],
-        [8, 0],
-        [8, 1],
-        [8, 2],
-      ],
-      [
-        [6, 3],
-        [6, 4],
-        [6, 5],
-        [7, 3],
-        [7, 4],
-        [7, 5],
-        [8, 3],
-        [8, 4],
-        [8, 5],
-      ],
-      [
-        [6, 6],
-        [6, 7],
-        [6, 8],
-        [7, 6],
-        [7, 7],
-        [7, 8],
-        [8, 6],
-        [8, 7],
-        [8, 8],
-      ],
-    ];
   }
 
   setCellValue(row, col, value) {
@@ -144,11 +43,19 @@ export default function SudokuComponent() {
 
     const subgridCol = Math.floor(col / 3);
     const subgridRow = Math.floor(row / 3);
-    const subgridIdx = subgridRow * 3 + subgridCol;
-    const subgrid = sudoku.subgrids[subgridIdx];
 
     // check subsgrid
-    if (subgrid.some(([r, c]) => sudoku.board[r][c] === event.key)) return;
+    for (let rowIdx = subgridRow * 3; rowIdx < (subgridRow + 1) * 3; rowIdx++) {
+      for (
+        let colIdx = subgridCol * 3;
+        colIdx < (subgridCol + 1) * 3;
+        colIdx++
+      ) {
+        if (sudoku.board[rowIdx][colIdx] === event.key) {
+          return;
+        }
+      }
+    }
 
     sudoku.setCellValue(row, col, event.key);
 
