74872cda0a7d597d9b865249b22262fa3235a4e6
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-07-28 08:50:52 -0700

    Update signup.js

--- frontend/pages/challenges/signup.js
+++ frontend/pages/challenges/signup.js
@@ -1,3 +1,16 @@
+function API(data) {
+  return new Promise((res) => {
+    setTimeout(
+      () =>
+        res({
+          success: "Your Account has been successfully created!",
+          error: "Username is taken",
+        }),
+      1000
+    );
+  });
+}
+
 export default function SignupForm() {
   return(
     <>
@@ -5,25 +18,16 @@ export default function SignupForm() {
         Signup Form
       </h1>
       <p>
-      Build a user Signup form in React with the following features. 
+        Build a user Signup form in React with the following features.
 
-      <ol>
-        <li>An email and a password input</li>
-        <li>Email must have an “@” and the domain side must include a “.”</li>
-        <li>Password must include
-          <ol>
-            <li>At least one special character</li>
-            <li>one number and be at least 8 characters</li>
-          </ol>
-        </li>
-        <li>Validation and error handling
-          <ol>
-            <li>Client-side validations</li>
-            <li>Server side errors</li>
-          </ol>
-        </li>
-        <li>Basic aesthetics with pure CSS</li>
-      </ol>
+        1. An email and a password input
+        2. Email must have an “@” and the domain side must include a “.”
+        3. Password must include
+            1.  at least one special character
+            2. one number and be at least 8 characters
+        4. Submission request handling  
+           1. Utilze the mock API function to handle submissions
+        5. Basic aesthetics with pure CSS
       </p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/M8fqHaJU_cc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </>
