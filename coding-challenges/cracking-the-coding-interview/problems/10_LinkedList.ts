7e1c9d63a45074e35586d86cba5413a3edddcfb6
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-08-06 18:38:45 -0300

    add challenge

---
+++ coding-challenges/cracking-the-coding-interview/problems/10_LinkedList.ts
@@ -0,0 +1,12 @@
+// 10. *Implement a Linked List*;
+
+// Create the data structure with the corresponding initial functions:
+// constructor
+// push()
+// head / tail accessors
+
+function isSubstring(s1: string, s2: string): boolean {
+  return s1.includes(s2);
+}
+
+export default function stringRotation(s1: string, s2: string): boolean {}
