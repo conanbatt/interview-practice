10b5f41ea71bf8e150a50e7a22f9c084072c4286
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-08-07 22:56:51 -0300

    Update 26_animalShelter.ts

--- coding-challenges/cracking-the-coding-interview/problems/26_animalShelter.ts
+++ coding-challenges/cracking-the-coding-interview/problems/26_animalShelter.ts
@@ -1,11 +1,22 @@
 // 6. *Animal Shelter*:
 
-// An animal shelter, which holds only dogs and cats, operates on a strictly "first in, first out" basis. People must adopt either the "oldest" (based on arrival time) of all animals at the shelter, or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type). They cannot select which specific animal they would like. Create the data structures to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat. You may use the built-in LinkedList data structure.
+// An animal shelter, which holds only dogs and cats, operates on a strictly
+// "first in, first out" basis. People must adopt either the "oldest"
+// (based on arrival time) of all animals at the shelter,
+// or they can select whether they would prefer a dog or a cat
+// (and will receive the oldest animal of that type).
+// They cannot select which specific animal they would like.
+// Create the data structures to maintain this system and implement operations
+// such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
+// You may use the built-in LinkedList data structure.
 
-export type AnimalType = 'dog' | 'cat';
+export type AnimalType = "dog" | "cat";
 
 export class Animal {
-
+  type: AnimalType;
+  constructor(type: AnimalType) {
+    this.type = type;
+  }
 }
 
 export default class AnimalShelter {
