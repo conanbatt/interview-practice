b360fa0823ae669637824b17b6409a6f57fbfebc
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-08-07 22:57:11 -0300

    Update 22_stackMin.ts

--- coding-challenges/cracking-the-coding-interview/problems/22_stackMin.ts
+++ coding-challenges/cracking-the-coding-interview/problems/22_stackMin.ts
@@ -1,4 +1,8 @@
-// 2. *Stack Min*: How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, pop, and min should all operate in O(1) time.
+// 2. *Stack Min*: How would you design a stack which,
+// in addition to push and pop,
+// has a function min which returns the minimum element?
+// Push, pop, and min should all operate in O(1) time.
+//
 
 export default class StackMin<T> {
     constructor() {
