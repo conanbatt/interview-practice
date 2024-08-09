e91fd5f1835fdbfbeb8e4009d9eb0f225ba3a195
Jenaro Calviño <jen.calvineo@gmail.com>
2024-08-09 12:21:34 -0300

    matrix rotation

--- coding-challenges/cracking-the-coding-interview/solutions/rotate-matrix.ts
+++ coding-challenges/cracking-the-coding-interview/solutions/rotate-matrix.ts
@@ -1,3 +1,10 @@
 export default function rotateMatrix(m: number[][]) {
-  return m;
+  const o = new Array(m.length).fill(0).map((_, i) => new Array(m[i].length));
+
+  for (let i = 0; i < m.length; i++) {
+    for (let j = 0; j < m[i].length; j++) {
+      o[j][m.length - i - 1] = m[i][j];
+    }
+  }
+  return o;
 }
