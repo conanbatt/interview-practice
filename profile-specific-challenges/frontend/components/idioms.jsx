afc036f19df457e41954be3bc3c07da5d3ff7c06
Ignacio Marenco <ignaciomarencodls@gmail.com>
2024-08-09 20:56:40 -0300

    fix: idioms

--- profile-specific-challenges/frontend/components/idioms.jsx
+++ profile-specific-challenges/frontend/components/idioms.jsx
@@ -9,40 +9,36 @@
 */
 import { useEffect } from 'react'
 
+// I remove the function and set the button directly in the return to avoid re render.
 export function FunctionsAsComponents({ buttonText = 'Start Now' }) {
-  const showButton = () => {
+  return (<div>
     <button>
       {buttonText}
     </button>
-  }
-
-  return (<div>
-    {showButton()}
   </div>)
 }
 
-export function objectCopying() {
-  const object = { a: { c: 1 }, b: 2 }
-  const copy = { ...object }
-  copy.a.c = 2
-  return { ...object }
+// I added the parameter object to make this function dinamically and also make a copy of the object.
+export function objectCopying(object) {
+  return Object.assign({}, object);
+}
+// I added the parameter array to make this function dinamically and also make a copy of the array.
+export function arrayCopying(array) {
+  return Array.from(array);
 }
 
-export function arrayCopying() {
-  const array = [{ a: 1}, {a: 2}, {a: 3}]
-  const copy = [...array]
-  return copy;
-}
 
-// user abort
-export function UseEffect({ fetchURL, label }) {
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetch(fetchURL);
-    };
-    
-    fetchData();
+export function UseEffect({
+  fetchURL,
+  label,
+}) {
+  const fetchData = useCallback(async () => {
+    await fetch(fetchURL);
   }, [fetchURL]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div>
@@ -51,16 +47,12 @@ export function UseEffect({ fetchURL, label }) {
   );
 }
 
+// I remove unnecessary code and avoid use of useState and useEffects.
 export function UseEffectDerivedCalculation() {
-  const [remainder, setReminder] = useState()
-  const [clickedTimes, setClickedTimes] = useState()
-
-  useEffect(() => {
-    setReminder(clickedTimes % 5)
-  }, [clickedTimes])
-
+  const [clickedTimes, setClickedTimes] = useState();
   const handleClick = () => setClickedTimes(clickedTimes + 1)
 
+  const reminder = clickedTimes % 5;
   return (
     <div>
       <button onClick={handleClick}>Add Click Count</button>
@@ -68,18 +60,15 @@ export function UseEffectDerivedCalculation() {
         {sum}
       </span>
       <span>
-        {remainder}
+        {reminder}
       </span>
     </div>
   )
 }
 
+// I remove unnecessary code and improve the use of lifecycle.
 export function UseEffectLifeCycle() {
-  const [_loaded, setLoaded] = useState()
-
-  useEffect(() => {
-    setTimeout(() => setLoaded(true), 1000)
-  }, [])
+  const [clickedTimes, setClickedTimes] = useState(0);
 
   const handleClick = () => setClickedTimes(clickedTimes + 1)
 
@@ -93,13 +82,16 @@ export function UseEffectLifeCycle() {
   )
 }
 
+// I add the clearInterval to kill the interval process once the component is unmounted.
 export function DirtyUnmount() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       setTime(t => t + 1)
     }, 1000)
+
+  return () => clearInterval(intervalId);
   }, [])
 
   return (
@@ -109,31 +101,33 @@ export function DirtyUnmount() {
   )
 }
 
+// I refactor the code to use useState and show the Mounted string only when it's mounted.
 export function AvoidingUseState() {
-  const ref = useRef('Unmounted');
+  const [value, setValue] = useState('Unmounted');
 
   useEffect(() => {
-    ref.current = "Mounted"
+    setValue("Mounted")
   }, [])
 
   return (
     <div>
-      {ref.current}
+      {value}
     </div>
   )
 }
 
 async function API() { return true }
 
+// I added the loading as react state and not as a common javascript variable, when we use common variables in the react state could cause issues.
 export function UntraceableState() {
   const [result, setResult] = useState()  
-  let loading = false
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      loading = true
+      setLoading(true);
       const result = await API();
-      loading = false;
+      setLoading(false);
       setResult(result);
     };
       
@@ -148,91 +142,102 @@ export function UntraceableState() {
   )
 }
 
+// I added a more dynamic way to create an array.
 export function CrudeDeclarations() {
-  const calendarDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  const calendarDays = Array(30).fill(0).map((_, i) => i + 1);
   return (<ol>
     {calendarDays.map((val) => <span key={val}>{val}</span>)}
   </ol>)
 }
 
+// I move the compare value outside the function and also move the conditional value outside the return.
+const AGE_LIMIT = 18;
 export function MagicNumbers(age) {
+  const isLessThanAge = age < AGE_LIMIT;
   return (<ol>
-    { age < 18 ? <div>Spicy</div> : <div>You are not old enough</div>}
+    {isLessThanAge ? <div>Spicy</div> : <div>You are not old enough</div>}
   </ol>)
 }
 
+//  I wrap the code in a form to have a better semantics in html.
 export function UnidiomaticHTMLStructure() {
-  const [name, setName] = useState("")
-  const handleSubmit = (e) => {}
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
-  return (<div>
-    <input value={name} name="name" type="text" onChange={setName} />
-    <button type="submit" onClick={handleSubmit}>Submit</button>
-  </div>)
-}
-
-export function CrudeStateManagement() {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
-  const [location, setLocation] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  
-
-  const handleSubmit = (e) => {}
-
-  return (<form onSubmit={handleSubmit}>
-    <input value={name} name="name" type="text" onChange={setName} />
-    <input value={age} name="age" type="number" onChange={setAge} />
-    <input value={location} name="location" type="text" onChange={setLocation} />
-    <input value={email} name="email" type="email" onChange={setEmail} />
-    <input value={password} name="password" type="password" onChange={setPassword} />
-    <button type="submit">Submit</button>
+  return (<form  onSubmit={handleSubmit} >
+    <input value={name} id="name" name="name" type="text" onChange={(event) => setName(event.target.value)} />
+    <button type="submit" />
   </form>)
 }
 
+// I refactor the code to only use one state related with the form and make the component more easy to understand.
+export function CrudeStateManagement() {
+  const [fields, setFields] = useState({})
+  const handleSubmit = (e) => {};
+
+  const onChangeField = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFields({...fields, [name]: value });
+  }
+
+  return (<form onSubmit={handleSubmit}>
+    <input value={fields?.name} name="name" type="text" onChange={onChangeField} />
+    <input value={fields?.age} name="age" type="number" onChange={onChangeField} />
+    <input value={fields?.location} name="location" type="text" onChange={onChangeField} />
+    <input value={fields?.email} name="email" type="email" onChange={onChangeField} />
+    <input value={fields?.password} name="password" type="password" onChange={onChangeField} />
+    <button type="submit">Submit</button>
+  </form>);
+}
+
+// I wrap the list in a ul/li structure to have a better semantics in the list and also concat the lists to iterate over the unique list and avoid key issues.
 export function UnidiomaticHTMLHierarchy() {
   const bids = [1,2,3]
   const asks = [1,2,3]
 
-  return (<li>
-    {bids.map((bid, i) => <span key={i}>{bid}</span>)}
-    {asks.map((ask, j) => <span key={j+'asks'}>{ask}</span>)}
-  </li>)
+  const list = bids.concat(asks);
+  return (<ul>
+    {list.map((item, i) => <li><span key={i}>{item}</span></li>)}
+  </ul>)
 }
 
+//
 export function SubstandardDataStructure() {
-  const [error, setError] = useState("")
+  const [errors, setErrors] = useState([])
 
   return(
     <div>
-      <button onClick={() => setError('Error A')}>Throw Error A</button>
-      <button onClick={() => setError('Error B')}>Throw Error B</button>
-      <button onClick={() => setError('')}>Clear Errors</button>
+      <button onClick={() => setErrors(prev => [...prev, 'Error A'])}>Throw Error A</button>
+      <button onClick={() => setErrors(prev => [...prev, 'Error B'])}>Throw Error B</button>
+      <button onClick={() => setErrors([])}>Clear Errors</button>
       <div>
-        {error}
+        {errors.map(error => <p>{error}</p>)}
       </div>
     </div>
   )
 }
 
+// I mainly add the right identifier to list of li to avoid issues with re render.
 export function DangerousIdentifier() {
   const [people, setPeople] = useState([])
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const person = new FormData(e.target);
-    setPeople(ppl => [...ppl, ...person])
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    setPeople(ppl => [...ppl, formData.get('name')])
   }
 
   return(
     <div>
       <form onSubmit={handleSubmit}>
         <input type="text" />
-        <button>Add Person</button>
+        <button type="submit">Add Person</button>
       </form>
       <ul>
-        {people.map(person => <span key={person.name}>{person.name}</span>)}
+        {people.map((person, i) => <li key={`${person.name}-${i}`}>{person.name}</li>)}
       </ul>
     </div>
   )
@@ -241,14 +246,14 @@ export function DangerousIdentifier() {
 async function fetchLeader() { return { name: 'Messi' }}
 async function fetchDetails(leader) { return { ...leader, country: 'Argentina' }}
 
-// Hint: this only requires a single line change!
+// I add the condition to only call enhanceRecord once we have the leader information.
 export function UnnecessaryEffectTriggering() {
-  const [leader, setLeader] = useState({})
+  const [leader, setLeader] = useState({});
 
   useEffect(() => {
     const interval = setInterval(async () => {
       const leader = await fetchLeader()
-      setLeader(leader)
+      setLeader(leader);
     }, 1000)
     return () => clearInterval(interval)
   }, [])
@@ -258,7 +263,9 @@ export function UnnecessaryEffectTriggering() {
       const enriched = await fetchDetails(leader);
       setLeader(enriched);
     }
-    enhanceRecord();
+    if (Object.keys(leader) > 0) {
+      enhanceRecord();
+    }
   }, [leader]); 
 
 
@@ -272,7 +279,7 @@ export function UnnecessaryEffectTriggering() {
 
 async function trackClick(ids) { return ids }
 
-// Hint: same error pattern as above
+// I add the key over li, to avoid issues with the re render of list.
 export function IncorrectDependencies(records) {
   const handleClick = useCallback(() => {
     trackClick(records);
@@ -280,49 +287,54 @@ export function IncorrectDependencies(records) {
 
   return(
     <div>
-      {records.map(record => <div id={record.id}>{record.name}</div>)}
+      {records.map(record => <div key={record.id} id={record.id}>{record.name}</div>)}
       <button onClick={handleClick}>Click me!</button>
     </div>
   )
 }
 
-export function UnnecessaryFunctionRedefinitions(emails) {
-  const validateEmail = (email) => email.includes("@")
+// I set outside of function component the function isValidEmail to avoid re defined in the render.
+const isValidEmail = (email) => email.includes("@");
 
+export function UnnecessaryFunctionRedefinitions(emails) {
   return(
     <div>
       {emails.map(email => (
         <div key={email}>
-          {email} is {validateEmail(email) ? 'Valid' : 'Invalid'}
+          {email} is {isValidEmail(email) ? 'Valid' : 'Invalid'}
         </div>
       ))}
     </div>
   )
 }
 
-
+// I set the requests in parallel to not wait for loading in a serial way and has the response more fast, 
+// also I concat the records to use the same list and not modify the state two times.
 async function fetchRecords() { return [{id: 1, type: 'record'}]}
 async function fetchAlternateRecords() { return [{ id: 1, type: 'alt-record' }]}
 
 export function SerialLoading() {
 
   const [records, setRecords] = useState([])
-  const [altRecords, setAltRecords] = useState([])
   
   useEffect(() => {
     async function loadRecords() {
-      const recs = await fetchRecords();
-      const altRecs = await fetchAlternateRecords();
-      setRecords(recs);
-      setAltRecords(altRecs);
+      const [recs, altRecs] = await Promise.all(
+        [
+        fetchRecords(),
+        fetchAlternateRecords(),
+      ]
+      );
+      setRecords(recs.concat(altRecs));
+
     }
+
     loadRecords();
   }, []);
 
   return(
     <div>
       {records.map(rec => <div key={rec.id}></div>)}
-      {altRecords.map(rec => <div key={rec.id}></div>)}
     </div>
   )
 }
@@ -330,8 +342,20 @@ export function SerialLoading() {
 async function fetchRecords() { return [{id: 1, type: 'record'}]}
 async function fetchAlternateRecords() { return [{ id: 1, type: 'alt-record' }]}
 
-// Hint: part of the rendering structure is re-rendered frequently unnecessarily
+// I movethe async call and effects to other component to not re render the entire component and only re render what will be updated.
 export function UnoptimizableRenderingStructure(altRecords) {
+
+  return(
+    <div>
+      <AsyncRecords />
+      <ul>
+        {altRecords.map(rec => <li key={rec.id}>{rec.id}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+function AsyncRecords() {
   const [records, setRecords] = useState([])
   
   useEffect(() => {
@@ -346,15 +370,5 @@ export function UnoptimizableRenderingStructure(altRecords) {
     loadRecords();
   }, []); 
 
-
-  return(
-    <div>
-      <ul>
-        {records.map(rec => <li key={rec.id}>{rec.id}</li>)}
-      </ul>
-      <ul>
-        {altRecords.map(rec => <li key={rec.id}>{rec.id}</li>)}
-      </ul>
-    </div>
-  )
+  return <ul>{records.map(rec => <li key={rec.id}>{rec.id}</li>)} </ul>
 }
