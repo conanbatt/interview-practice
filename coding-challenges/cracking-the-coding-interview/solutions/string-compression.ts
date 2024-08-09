eca5048b96cdd1c4753a7ee7b76b7d08d2fc7ac4
Jenaro Calviño <jen.calvineo@gmail.com>
2024-08-09 12:09:47 -0300

    alt string compression

--- coding-challenges/cracking-the-coding-interview/solutions/string-compression.ts
+++ coding-challenges/cracking-the-coding-interview/solutions/string-compression.ts
@@ -6,21 +6,39 @@ export default function stringCompression(str: string) {
 
   if (str.length === 1) return str;
 
-  for (let i = 1; i < str.length; i++) {
-    const char = str[i];
-    let prev = str[i - 1];
+  // for (let i = 1; i < str.length; i++) {
+  //   const char = str[i];
+  //   let prev = str[i - 1];
+
+  //   if (prev !== char) {
+  //     output = `${output}${prev}${count}`;
+  //     count = 1;
+  //   } else if (prev === char) {
+  //     count++;
+  //   }
+
+  //   if (i === str.length - 1) {
+  //     output = `${output}${char}${count}`;
+  //   }
+  // }
+
+  const a = str.split("");
+  let prev = a.shift();
+  while (a.length) {
+    let char = a.shift();
 
     if (prev !== char) {
-      output = `${output}${prev}${count}`;
+      output += `${prev}${count}`;
       count = 1;
-    } else if (prev === char) {
+    } else {
       count++;
     }
 
-    if (i === str.length - 1) {
-      output = `${output}${char}${count}`;
-    }
+    prev = char;
   }
+
+  // add last
+  output += `${prev}${count}`;
 
   return output.length > str.length ? str : output;
 }
