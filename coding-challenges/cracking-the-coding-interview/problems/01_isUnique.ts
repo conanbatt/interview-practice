56fb16fdb6239ad4d8cb39d3a5fd199703bb5f3b
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-08-11 17:00:53 -0300

    remove jest for vitest

--- coding-challenges/cracking-the-coding-interview/problems/01_isUnique.ts
+++ coding-challenges/cracking-the-coding-interview/problems/01_isUnique.ts
@@ -3,6 +3,4 @@
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
 
-export default function isUnique (str : string): boolean {
-
-}
+export default function isUnique(str: string): boolean {}
