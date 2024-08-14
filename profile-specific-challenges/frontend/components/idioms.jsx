7213148cca419c9e486929669a482e50734eb3ff
Valentin Fernandez <valfernandez@Valentins-MBP.fibertel.com.ar>
2024-08-14 13:26:25 -0300

    fix: idioms

--- profile-specific-challenges/frontend/components/idioms.jsx
+++ profile-specific-challenges/frontend/components/idioms.jsx
@@ -7,20 +7,20 @@
   Some references used:
   https://claritydev.net/blog/the-most-common-mistakes-when-using-react
 */
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
+
+const Button = ({children}) => <button>{children}</button>
 
 export function FunctionsAsComponents({ buttonText = 'Start Now' }) {
-  const showButton = () => {
-    <button>
-      {buttonText}
-    </button>
-  }
-
+  // moving the button to it's own component will prevent the function from recreating it on every render
   return (<div>
-    {showButton()}
+    <Button>{buttonText}</Button>
   </div>)
 }
 
+// the issue here is the shallow copy of the spread operator.
+// you can either use a library like lodash or the classical JSON.parse(JSON.stringify(...))
+// the JSON API solution won't work for non-serializable attributes
 export function objectCopying() {
   const object = { a: { c: 1 }, b: 2 }
   const copy = { ...object }
@@ -28,6 +28,7 @@ export function objectCopying() {
   return { ...object }
 }
 
+// same as above, objects in both arrays share the same memory address respectively due to the shallow copy
 export function arrayCopying() {
   const array = [{ a: 1}, {a: 2}, {a: 3}]
   const copy = [...array]
@@ -37,11 +38,16 @@ export function arrayCopying() {
 // user abort
 export function UseEffect({ fetchURL, label }) {
   useEffect(() => {
+    const abortController = new AbortController();
+    // the request can be aborted on unmount
     const fetchData = async () => {
-      await fetch(fetchURL);
+      await fetch(fetchURL, {signal: abortController.signal});
     };
     
     fetchData();
+    return () => {
+      abortController.abort();
+    }
   }, [fetchURL]);
 
   return (
@@ -52,12 +58,13 @@ export function UseEffect({ fetchURL, label }) {
 }
 
 export function UseEffectDerivedCalculation() {
-  const [remainder, setReminder] = useState()
   const [clickedTimes, setClickedTimes] = useState()
 
-  useEffect(() => {
-    setReminder(clickedTimes % 5)
-  }, [clickedTimes])
+  // memoizing this might be an overkill.
+  // the useEffect will trigger the reconciliation algorithm twice, one after setting the clickedItems state
+  // and one after setting the remainder state.
+  // we saved some commits with this derived state
+  const remainder = useMemo(() => clickedTimes % 5, [clickedTimes]);
 
   const handleClick = () => setClickedTimes(clickedTimes + 1)
 
@@ -74,6 +81,7 @@ export function UseEffectDerivedCalculation() {
   )
 }
 
+// not sure what's going on with this example 😛
 export function UseEffectLifeCycle() {
   const [_loaded, setLoaded] = useState()
 
@@ -93,13 +101,17 @@ export function UseEffectLifeCycle() {
   )
 }
 
+// this interval needs to get cleared as you might end up updating the state of an unmounted component
 export function DirtyUnmount() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(t => t + 1)
-    }, 1000)
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    }
   }, [])
 
   return (
@@ -108,7 +120,8 @@ export function DirtyUnmount() {
     </div>
   )
 }
-
+// triggers for the reconciliation algorithm are mainly: state changes or prop changes
+// this is not achievable as a change in a ref won't make the component re-render, hence not updating the UI
 export function AvoidingUseState() {
   const ref = useRef('Unmounted');
 
@@ -124,16 +137,16 @@ export function AvoidingUseState() {
 }
 
 async function API() { return true }
-
+// the loading can either: be a derived state or be a state on it's own
 export function UntraceableState() {
-  const [result, setResult] = useState()  
-  let loading = false
+  const [result, setResult] = useState(null)  
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      loading = true
+      setLoading(true);
       const result = await API();
-      loading = false;
+      setLoading(false);
       setResult(result);
     };
       
@@ -147,82 +160,99 @@ export function UntraceableState() {
     </div>
   )
 }
-
+// any object has to be outside the component's declaration as it will be recreated on every render
+// if the object is compound of any state or props we would need to go in the memoization path
+const calendarDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 export function CrudeDeclarations() {
-  const calendarDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
   return (<ol>
-    {calendarDays.map((val) => <span key={val}>{val}</span>)}
+    {/*also, keep the html semantic for a11y*/}
+    {calendarDays.map((val) => <li key={val}>{val}</li>)}
   </ol>)
 }
 
+const LEGAL_AGE = 18;
+const isUnderage = (age) => age < LEGAL_AGE;
+// improves code readability 
 export function MagicNumbers(age) {
   return (<ol>
-    { age < 18 ? <div>Spicy</div> : <div>You are not old enough</div>}
+    { !isUnderage(age) ? <div>Spicy</div> : <div>You are not old enough</div>}
   </ol>)
 }
 
+// adds semantic HTML tags that improves a11y and takes advantage of native component's APIs
 export function UnidiomaticHTMLStructure() {
   const [name, setName] = useState("")
   const handleSubmit = (e) => {}
 
-  return (<div>
-    <input value={name} name="name" type="text" onChange={setName} />
+  return (<form onSubmit={handleSubmit}>
+    <input value={name} name="name" type="text" onChange={(e) => setName(e.target.value)} />
     <button type="submit" onClick={handleSubmit}>Submit</button>
-  </div>)
+  </form>)
 }
 
 export function CrudeStateManagement() {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
-  const [location, setLocation] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  
-
+  // here we can have all fields in a single state
+  const [formData, setFormData] = useState({
+    name: '', 
+    age: '', 
+    location: '',
+    email: '',
+    password: '',
+  })
   const handleSubmit = (e) => {}
 
+  // create a single onChange handler
+  const onChange = (e) => setFormData(prev => ({...prev, [e.target.name]: e.target.value}))
+
   return (<form onSubmit={handleSubmit}>
-    <input value={name} name="name" type="text" onChange={setName} />
-    <input value={age} name="age" type="number" onChange={setAge} />
-    <input value={location} name="location" type="text" onChange={setLocation} />
-    <input value={email} name="email" type="email" onChange={setEmail} />
-    <input value={password} name="password" type="password" onChange={setPassword} />
+    <input value={formData.name} name="name" type="text" onChange={onChange} />
+    <input value={formData.age} name="age" type="number" onChange={onChange} />
+    <input value={formData.location} name="location" type="text" onChange={onChange} />
+    <input value={formData.email} name="email" type="email" onChange={onChange} />
+    <input value={formData.password} name="password" type="password" onChange={onChange} />
     <button type="submit">Submit</button>
   </form>)
 }
 
+const bids = [1,2,3]
+const asks = [1,2,3]
 export function UnidiomaticHTMLHierarchy() {
-  const bids = [1,2,3]
-  const asks = [1,2,3]
-
-  return (<li>
-    {bids.map((bid, i) => <span key={i}>{bid}</span>)}
-    {asks.map((ask, j) => <span key={j+'asks'}>{ask}</span>)}
-  </li>)
+  // we should be careful using array indexes as keys
+  return (
+  <>
+    <ul>
+      {bids.map((bid, i) => <li key={i}>{bid}</li>)}
+      {asks.map((ask, j) => <li key={j+'asks'}>{ask}</li>)}
+    </ul>
+  </>
+  )
 }
 
+// I guess this is what the example is looking for
 export function SubstandardDataStructure() {
-  const [error, setError] = useState("")
+  const [error, setError] = useState([])
 
   return(
     <div>
-      <button onClick={() => setError('Error A')}>Throw Error A</button>
-      <button onClick={() => setError('Error B')}>Throw Error B</button>
-      <button onClick={() => setError('')}>Clear Errors</button>
+      <button onClick={() => setError(prev => ([...prev, 'Error A']))}>Throw Error A</button>
+      <button onClick={() => setError(prev => ([...prev, 'Error B']))}>Throw Error B</button>
+      <button onClick={() => setError([])}>Clear Errors</button>
       <div>
-        {error}
+        {error.map((err, i)=> <li key={i}>{err}</li>)}
       </div>
     </div>
   )
 }
 
+// apart from handling the FormData wrong (I don't think this was intentional)
+// the key for each list item must be unique. Names are not unique
 export function DangerousIdentifier() {
   const [people, setPeople] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const person = new FormData(e.target);
-    setPeople(ppl => [...ppl, ...person])
+    setPeople(ppl => ([...ppl, ...person]))
   }
 
   return(
@@ -232,7 +262,7 @@ export function DangerousIdentifier() {
         <button>Add Person</button>
       </form>
       <ul>
-        {people.map(person => <span key={person.name}>{person.name}</span>)}
+        {people.map(person => <li key={person.name}>{person.name}</li>)}
       </ul>
     </div>
   )
@@ -247,8 +277,9 @@ export function UnnecessaryEffectTriggering() {
 
   useEffect(() => {
     const interval = setInterval(async () => {
+      // next line is returning a new object every time,
       const leader = await fetchLeader()
-      setLeader(leader)
+      setLeader(prev => leader.name !== prev.name ? leader : prev)
     }, 1000)
     return () => clearInterval(interval)
   }, [])
@@ -265,7 +296,7 @@ export function UnnecessaryEffectTriggering() {
   return(
     <div>
       <div>Leader:{leader.name}</div>
-      {leader.country && <div>{`From: ${leader.country}`}</div>}
+      {leader.country && <div>{`From: ${leader.country}`}</div>} {/* this will be inconsistent for a few secs, there should be a loading state */}
     </div>
   )
 }
@@ -273,21 +304,22 @@ export function UnnecessaryEffectTriggering() {
 async function trackClick(ids) { return ids }
 
 // Hint: same error pattern as above
-export function IncorrectDependencies(records) {
+// LGTM
+export function IncorrectDependencies({records}) {
   const handleClick = useCallback(() => {
     trackClick(records);
   }, [records]);
 
   return(
     <div>
-      {records.map(record => <div id={record.id}>{record.name}</div>)}
+      {records.map(record => <div key={record.id}>{record.name}</div>)}
       <button onClick={handleClick}>Click me!</button>
     </div>
   )
 }
-
-export function UnnecessaryFunctionRedefinitions(emails) {
-  const validateEmail = (email) => email.includes("@")
+// avoid redefining the fn in every render
+const validateEmail = (email) => email.includes("@")
+export function UnnecessaryFunctionRedefinitions({emails}) {
 
   return(
     <div>
@@ -311,10 +343,10 @@ export function SerialLoading() {
   
   useEffect(() => {
     async function loadRecords() {
-      const recs = await fetchRecords();
-      const altRecs = await fetchAlternateRecords();
-      setRecords(recs);
-      setAltRecords(altRecs);
+      // parallelize 
+      const [rec, alternateRec] = await Promise.all([fetchRecords(), fetchAlternateRecords()]);
+      setRecords(rec);
+      setAltRecords(alternateRec);
     }
     loadRecords();
   }, []);
@@ -346,15 +378,19 @@ export function UnoptimizableRenderingStructure(altRecords) {
     loadRecords();
   }, []); 
 
+  // you can either do this or use the React.memo HOC by creating another component
+  const memoizedAltRecords = useMemo(() => (
+    <ul>
+      {altRecords.map(rec => <li key={rec.id}>{rec.id}</li>)}
+    </ul>
+  ), [altRecords]);
 
   return(
     <div>
       <ul>
         {records.map(rec => <li key={rec.id}>{rec.id}</li>)}
       </ul>
-      <ul>
-        {altRecords.map(rec => <li key={rec.id}>{rec.id}</li>)}
-      </ul>
+      {memoizedAltRecords}
     </div>
   )
 }
