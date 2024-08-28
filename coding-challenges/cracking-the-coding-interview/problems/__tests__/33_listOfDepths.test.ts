9a35f806c38aa63aec8b525b794af56c910ecb89
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-08-27 22:06:22 -0300

    fixing some tests

--- coding-challenges/cracking-the-coding-interview/problems/__tests__/33_listOfDepths.test.ts
+++ coding-challenges/cracking-the-coding-interview/problems/__tests__/33_listOfDepths.test.ts
@@ -1,8 +1,8 @@
 import listOfDepths, { TreeNode, ListNode } from "../33_listOfDepths";
 
-describe('listOfDepths', () => {
-    test('creates linked lists of nodes at each depth', () => {
-        /*
+describe("listOfDepths", () => {
+  test("creates linked lists of nodes at each depth", () => {
+    /*
                     1
                    / \
                   2   3
@@ -11,35 +11,35 @@ describe('listOfDepths', () => {
                /
               7
         */
-        const root: TreeNode<number> = {
-            value: 1,
-            left: {
-                value: 2,
-                left: {
-                    value: 4,
-                    left: { value: 7 }
-                },
-                right: { value: 5 }
-            },
-            right: {
-                value: 3,
-                right: { value: 6 }
-            }
-        };
+    const root: TreeNode<number> = {
+      value: 1,
+      left: {
+        value: 2,
+        left: {
+          value: 4,
+          left: { value: 7 },
+        },
+        right: { value: 5 },
+      },
+      right: {
+        value: 3,
+        right: { value: 6 },
+      },
+    };
 
-        const expectedLists: ListNode<number>[] = [
-            { value: 1,  }, // Depth 0: [1]
-            { value: 2, next: { value: 3, next: undefined } }, // Depth 1: [2, 3]
-            { value: 4, next: { value: 5, next: { value: 6, next: undefined } } }, // Depth 2: [4, 5, 6]
-            { value: 7, next: undefined } // Depth 3: [7]
-        ];
+    const expectedLists: ListNode<number>[] = [
+      { value: 1 }, // Depth 0: [1]
+      { value: 2, next: { value: 3 } }, // Depth 1: [2, 3]
+      { value: 4, next: { value: 5, next: { value: 6 } } }, // Depth 2: [4, 5, 6]
+      { value: 7 }, // Depth 3: [7]
+    ];
 
-        expect(listOfDepths(root)).toEqual(expectedLists);
-    });
+    expect(listOfDepths(root)).toEqual(expectedLists);
+  });
 
-    test('creates linked lists for single node tree', () => {
-        const root: TreeNode<number> = { value: 1 };
-        const expectedList: ListNode<ListNode<number>>[] = [{ value: { value: 1, next: undefined } }];
-        expect(listOfDepths(root)).toEqual(expectedList);
-    });
+  test("creates linked lists for single node tree", () => {
+    const root: TreeNode<number> = { value: 1 };
+    const expectedList: Array<ListNode<number>> = [{ value: 1 }];
+    expect(listOfDepths(root)).toEqual(expectedList);
+  });
 });
--- coding-challenges/cracking-the-coding-interview/problems/__tests__/37_buildOrder.test.ts
+++ coding-challenges/cracking-the-coding-interview/problems/__tests__/37_buildOrder.test.ts
@@ -1,25 +1,48 @@
 import buildOrder from "../37_buildOrder";
 
-describe('buildOrder', () => {
-    test('returns correct build order for valid input', () => {
-        const projects1 = ['a', 'b', 'c', 'd', 'e', 'f'];
-        const dependencies1 = [['a', 'd'], ['f', 'b'], ['b', 'd'], ['f', 'a'], ['d', 'c']];
-        expect(buildOrder(projects1, dependencies1)).toEqual(['e', 'f', 'b', 'a', 'd', 'c']);
+describe("buildOrder", () => {
+  test("returns correct build order for valid input", () => {
+    const projects1 = ["a", "b", "c", "d", "e", "f"];
+    const dependencies1 = [
+      ["a", "d"],
+      ["f", "b"],
+      ["b", "d"],
+      ["f", "a"],
+      ["d", "c"],
+    ];
+    expect(buildOrder(projects1, dependencies1)).toEqual([
+      "e",
+      "f",
+      "a",
+      "b",
+      "d",
+      "c",
+    ]);
+  });
 
-        const projects2 = ['a', 'b', 'c', 'd', 'e'];
-        const dependencies2 = [['a', 'd'], ['f', 'b'], ['b', 'd'], ['f', 'a'], ['d', 'c']];
-        expect(buildOrder(projects2, dependencies2)).toEqual('Error: No valid build order exists.');
-    });
+  test("throws error for no valid order", () => {
+    const projects = ["a", "b", "c", "d", "e"];
+    const dependencies = [
+      ["a", "d"],
+      ["f", "b"],
+      ["b", "d"],
+      ["f", "a"],
+      ["d", "c"],
+    ];
+    expect(() => buildOrder(projects, dependencies)).toThrowError(
+      /No valid build order exists/,
+    );
+  });
 
-    test('returns correct build order for single project', () => {
-        const projects = ['a'];
-        const dependencies: [string, string][] = [];
-        expect(buildOrder(projects, dependencies)).toEqual(['a']);
-    });
+  test("returns correct build order for single project", () => {
+    const projects = ["a"];
+    const dependencies: [string, string][] = [];
+    expect(buildOrder(projects, dependencies)).toEqual(["a"]);
+  });
 
-    test('returns correct build order for empty input', () => {
-        const projects: string[] = [];
-        const dependencies: [string, string][] = [];
-        expect(buildOrder(projects, dependencies)).toEqual([]);
-    });
+  test("returns correct build order for empty input", () => {
+    const projects: string[] = [];
+    const dependencies: [string, string][] = [];
+    expect(buildOrder(projects, dependencies)).toEqual([]);
+  });
 });
