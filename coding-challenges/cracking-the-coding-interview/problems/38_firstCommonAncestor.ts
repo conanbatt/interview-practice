61a7e0ff7203274db09c4c30e4e80cd0a5db3a12
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-08-28 23:05:32 -0300

    fix fca

--- coding-challenges/cracking-the-coding-interview/problems/38_firstCommonAncestor.ts
+++ coding-challenges/cracking-the-coding-interview/problems/38_firstCommonAncestor.ts
@@ -1,15 +1,19 @@
-// 8. *First Common Ancestor*: 
+// 8. *First Common Ancestor*:
 
-// Design an algorithm and write code to find the first common ancestor of two nodes 
-// in a binary tree. Avoid storing additional nodes in a data structure. 
+// Design an algorithm and write code to find the first common ancestor of two nodes
+// in a binary tree. Avoid storing additional nodes in a data structure.
 // NOTE: This is not necessarily a binary search tree.
 
 export type TreeNode<T> = {
-    value: T;
-    left?: TreeNode<T>;
-    right?: TreeNode<T>;
+  value: T;
+  left?: TreeNode<T>;
+  right?: TreeNode<T>;
 };
 
-export default function firstCommonAncestor<T>(root?: TreeNode<T> | null, p?: TreeNode<T>, q?: TreeNode<T>): TreeNode<T> | null | undefined {
+export default function firstCommonAncestor<T>(
+  root: TreeNode<T> | undefined,
+  p: TreeNode<T>,
+  q: TreeNode<T>,
+): TreeNode<T> | undefined {
 
 }
--- coding-challenges/cracking-the-coding-interview/problems/__tests__/38_firstCommonAncestor.test.ts
+++ coding-challenges/cracking-the-coding-interview/problems/__tests__/38_firstCommonAncestor.test.ts
@@ -1,33 +1,33 @@
 import firstCommonAncestor, { TreeNode } from "../38_firstCommonAncestor";
 
-const createNode = <T>(value: T, left?: TreeNode<T>, right?: TreeNode<T>): TreeNode<T> => {
-    return { value, left, right };
+const createNode = <T>(
+  value: T,
+  left?: TreeNode<T>,
+  right?: TreeNode<T>,
+): TreeNode<T> => {
+  return { value, left, right };
 };
 
-describe('firstCommonAncestor', () => {
-    test('returns correct common ancestor for valid input', () => {
-        /*
+describe("firstCommonAncestor", () => {
+  test("returns correct common ancestor for valid input", () => {
+    /*
                    1
                   / \
                  2   3
                 / \ / \
                4  5 6  7
         */
-        const root: TreeNode<number> = createNode(1,
-            createNode(2,
-                createNode(4),
-                createNode(5)),
-            createNode(3,
-                createNode(6),
-                createNode(7))
-        );
-        expect(firstCommonAncestor(root, root.left!, root.right!)).toBe(root); // Common ancestor of 2 and 3 is 1
-        expect(firstCommonAncestor(root, root.left!.left!, root.left!.right!)).toBe(root.left); // Common ancestor of 4 and 5 is 2
-        expect(firstCommonAncestor(root, root.left!.left!, root.right!.right!)).toBe(root); // Common ancestor of 4 and 7 is 1
-    });
-
-    test('returns null for invalid input', () => {
-        const root: TreeNode<number> | null = null;
-        expect(firstCommonAncestor(root, {} as TreeNode<number>, {} as TreeNode<number>)).toBe(null); // Empty tree
-    });
+    const root: TreeNode<number> = createNode(
+      1,
+      createNode(2, createNode(4), createNode(5)),
+      createNode(3, createNode(6), createNode(7)),
+    );
+    expect(firstCommonAncestor(root, root.left!, root.right!)).toBe(root); // Common ancestor of 2 and 3 is 1
+    expect(firstCommonAncestor(root, root.left!.left!, root.left!.right!)).toBe(
+      root.left,
+    ); // Common ancestor of 4 and 5 is 2
+    expect(
+      firstCommonAncestor(root, root.left!.left!, root.right!.right!),
+    ).toBe(root); // Common ancestor of 4 and 7 is 1
+  });
 });
