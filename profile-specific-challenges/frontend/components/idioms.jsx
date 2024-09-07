76cae21f57406a7dcae2334aa3aed945304cf3b0
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-09-07 17:17:52 -0300

    patch idioms

--- profile-specific-challenges/frontend/components/idioms.jsx
+++ profile-specific-challenges/frontend/components/idioms.jsx
@@ -1,46 +1,41 @@
 /*
   The exercise is meant to test code fluency, understanding of react idioms, and communication.
   To solve, rewrite the snippet of code to a better version and a description of why the code is improved.
-  
+
   Submit a fork or a PR to gabriel@silver.dev for feedback and corrections.
 
   Some references used:
   https://claritydev.net/blog/the-most-common-mistakes-when-using-react
 */
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-export function FunctionsAsComponents({ buttonText = 'Start Now' }) {
+export function FunctionsAsComponents({ buttonText = "Start Now" }) {
   const showButton = () => {
-    <button>
-      {buttonText}
-    </button>
-  }
+    <button>{buttonText}</button>;
+  };
 
-  return (<div>
-    {showButton()}
-  </div>)
+  return <div>{showButton()}</div>;
 }
 
-export function objectCopying() {
-  const object = { a: { c: 1 }, b: 2 }
-  const copy = { ...object }
-  copy.a.c = 2
-  return { ...object }
+export function objectShallowCopying() {
+  const object = { a: { c: 1 }, b: 2 };
+  const copy = { ...object };
+  copy.a.c = 2;
+  return { ...object };
 }
 
-export function arrayCopying() {
-  const array = [{ a: 1}, {a: 2}, {a: 3}]
-  const copy = [...array]
+export function arrayShallowCopying() {
+  const array = [{ a: 1 }, { a: 2 }, { a: 3 }];
+  const copy = [...array];
   return copy;
 }
 
-// user abort
-export function UseEffect({ fetchURL, label }) {
+export function UseEffectThrashing({ fetchURL, label }) {
   useEffect(() => {
     const fetchData = async () => {
       await fetch(fetchURL);
     };
-    
+
     fetchData();
   }, [fetchURL]);
 
@@ -52,45 +47,40 @@ export function UseEffect({ fetchURL, label }) {
 }
 
 export function UseEffectDerivedCalculation() {
-  const [remainder, setReminder] = useState()
-  const [clickedTimes, setClickedTimes] = useState()
+  const [remainder, setReminder] = useState();
+  const [clickedTimes, setClickedTimes] = useState();
 
   useEffect(() => {
-    setReminder(clickedTimes % 5)
-  }, [clickedTimes])
+    setReminder(clickedTimes % 5);
+  }, [clickedTimes]);
 
-  const handleClick = () => setClickedTimes(clickedTimes + 1)
+  const handleClick = () => setClickedTimes(clickedTimes + 1);
 
   return (
     <div>
       <button onClick={handleClick}>Add Click Count</button>
-      <span>
-        {sum}
-      </span>
-      <span>
-        {remainder}
-      </span>
+      <span>{sum}</span>
+      <span>{remainder}</span>
     </div>
-  )
+  );
 }
 
-export function UseEffectLifeCycle() {
-  const [_loaded, setLoaded] = useState()
+export function UseStateDerivedCalculation() {
+  const [remainder, setReminder] = useState();
+  const [clickedTimes, setClickedTimes] = useState();
 
-  useEffect(() => {
-    setTimeout(() => setLoaded(true), 1000)
-  }, [])
-
-  const handleClick = () => setClickedTimes(clickedTimes + 1)
+  const handleClick = () => {
+    setClickedTimes(clickedTimes + 1);
+    setReminder(clickedTimes % 5);
+  };
 
   return (
     <div>
       <button onClick={handleClick}>Add Click Count</button>
-      <span>
-        {clickedTimes}
-      </span>
+      <span>{sum}</span>
+      <span>{remainder}</span>
     </div>
-  )
+  );
 }
 
 export function DirtyUnmount() {
@@ -98,45 +88,39 @@ export function DirtyUnmount() {
 
   useEffect(() => {
     setInterval(() => {
-      setTime(t => t + 1)
-    }, 1000)
-  }, [])
+      setTime((t) => t + 1);
+    }, 1000);
+  }, []);
 
-  return (
-    <div>
-      Clock in seconds: {time}
-    </div>
-  )
+  return <div>Clock in seconds: {time}</div>;
 }
 
 export function AvoidingUseState() {
-  const ref = useRef('Unmounted');
+  const ref = useRef("Unmounted");
 
   useEffect(() => {
-    ref.current = "Mounted"
-  }, [])
+    ref.current = "Mounted";
+  }, []);
 
-  return (
-    <div>
-      {ref.current}
-    </div>
-  )
+  return <div>{ref.current}</div>;
 }
 
-async function API() { return true }
+async function API() {
+  return true;
+}
 
-export function UntraceableState() {
-  const [result, setResult] = useState()  
-  let loading = false
+export function UnrenderableState() {
+  const [result, setResult] = useState();
+  let loading = false;
 
   useEffect(() => {
     const fetchData = async () => {
-      loading = true
+      loading = true;
       const result = await API();
       loading = false;
       setResult(result);
     };
-      
+
     fetchData();
   }, []);
 
@@ -145,113 +129,145 @@ export function UntraceableState() {
       <span>Loading: {loading}</span>
       Result:{result}
     </div>
-  )
+  );
 }
 
 export function CrudeDeclarations() {
-  const calendarDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-  return (<ol>
-    {calendarDays.map((val) => <span key={val}>{val}</span>)}
-  </ol>)
+  const calendarDays = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
+  ];
+  return (
+    <ol>
+      {calendarDays.map((val) => (
+        <span key={val}>{val}</span>
+      ))}
+    </ol>
+  );
 }
 
 export function MagicNumbers(age) {
-  return (<ol>
-    { age < 18 ? <div>Spicy</div> : <div>You are not old enough</div>}
-  </ol>)
+  return (
+    <ol>{age < 18 ? <div>Spicy</div> : <div>You are not old enough</div>}</ol>
+  );
 }
 
 export function UnidiomaticHTMLStructure() {
-  const [name, setName] = useState("")
-  const handleSubmit = (e) => {}
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {};
 
-  return (<div>
-    <input value={name} name="name" type="text" onChange={setName} />
-    <button type="submit" onClick={handleSubmit}>Submit</button>
-  </div>)
+  return (
+    <div>
+      <input value={name} name="name" type="text" onChange={setName} />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export function CrudeStateManagement() {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
-  const [location, setLocation] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [location, setLocation] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {};
 
-  return (<form onSubmit={handleSubmit}>
-    <input value={name} name="name" type="text" onChange={setName} />
-    <input value={age} name="age" type="number" onChange={setAge} />
-    <input value={location} name="location" type="text" onChange={setLocation} />
-    <input value={email} name="email" type="email" onChange={setEmail} />
-    <input value={password} name="password" type="password" onChange={setPassword} />
-    <button type="submit">Submit</button>
-  </form>)
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={name} name="name" type="text" onChange={setName} />
+      <input value={age} name="age" type="number" onChange={setAge} />
+      <input
+        value={location}
+        name="location"
+        type="text"
+        onChange={setLocation}
+      />
+      <input value={email} name="email" type="email" onChange={setEmail} />
+      <input
+        value={password}
+        name="password"
+        type="password"
+        onChange={setPassword}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export function UnidiomaticHTMLHierarchy() {
-  const bids = [1,2,3]
-  const asks = [1,2,3]
+  const bids = [1, 2, 3];
+  const asks = [1, 2, 3];
 
-  return (<li>
-    {bids.map((bid, i) => <span key={i}>{bid}</span>)}
-    {asks.map((ask, j) => <span key={j+'asks'}>{ask}</span>)}
-  </li>)
+  return (
+    <li>
+      {bids.map((bid, i) => (
+        <span key={i}>{bid}</span>
+      ))}
+      {asks.map((ask, j) => (
+        <span key={j + "asks"}>{ask}</span>
+      ))}
+    </li>
+  );
 }
 
 export function SubstandardDataStructure() {
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
 
-  return(
+  return (
     <div>
-      <button onClick={() => setError('Error A')}>Throw Error A</button>
-      <button onClick={() => setError('Error B')}>Throw Error B</button>
-      <button onClick={() => setError('')}>Clear Errors</button>
-      <div>
-        {error}
-      </div>
+      <button onClick={() => setError("Error A")}>Throw Error A</button>
+      <button onClick={() => setError("Error B")}>Throw Error B</button>
+      <button onClick={() => setError("")}>Clear Errors</button>
+      <div>{error}</div>
     </div>
-  )
+  );
 }
 
 export function DangerousIdentifier() {
-  const [people, setPeople] = useState([])
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const person = new FormData(e.target);
-    setPeople(ppl => [...ppl, ...person])
-  }
+    setPeople((ppl) => [...ppl, ...person]);
+  };
 
-  return(
+  return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type="text" />
         <button>Add Person</button>
       </form>
       <ul>
-        {people.map(person => <span key={person.name}>{person.name}</span>)}
+        {people.map((person) => (
+          <span key={person.name}>{person.name}</span>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-async function fetchLeader() { return { name: 'Messi' }}
-async function fetchDetails(leader) { return { ...leader, country: 'Argentina' }}
+async function fetchLeader() {
+  return { name: "Messi" };
+}
+async function fetchDetails(leader) {
+  return { ...leader, country: "Argentina" };
+}
 
 // Hint: this only requires a single line change!
 export function UnnecessaryEffectTriggering() {
-  const [leader, setLeader] = useState({})
+  const [leader, setLeader] = useState({});
 
   useEffect(() => {
     const interval = setInterval(async () => {
-      const leader = await fetchLeader()
-      setLeader(leader)
-    }, 1000)
-    return () => clearInterval(interval)
-  }, [])
+      const leader = await fetchLeader();
+      setLeader(leader);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
     async function enhanceRecord() {
@@ -259,18 +275,19 @@ export function UnnecessaryEffectTriggering() {
       setLeader(enriched);
     }
     enhanceRecord();
-  }, [leader]); 
+  }, [leader]);
 
-
-  return(
+  return (
     <div>
       <div>Leader:{leader.name}</div>
       {leader.country && <div>{`From: ${leader.country}`}</div>}
     </div>
-  )
+  );
 }
 
-async function trackClick(ids) { return ids }
+async function trackClick(ids) {
+  return ids;
+}
 
 // Hint: same error pattern as above
 export function IncorrectDependencies(records) {
@@ -278,37 +295,41 @@ export function IncorrectDependencies(records) {
     trackClick(records);
   }, [records]);
 
-  return(
+  return (
     <div>
-      {records.map(record => <div id={record.id}>{record.name}</div>)}
+      {records.map((record) => (
+        <div id={record.id}>{record.name}</div>
+      ))}
       <button onClick={handleClick}>Click me!</button>
     </div>
-  )
+  );
 }
 
 export function UnnecessaryFunctionRedefinitions(emails) {
-  const validateEmail = (email) => email.includes("@")
+  const validateEmail = (email) => email.includes("@");
 
-  return(
+  return (
     <div>
-      {emails.map(email => (
+      {emails.map((email) => (
         <div key={email}>
-          {email} is {validateEmail(email) ? 'Valid' : 'Invalid'}
+          {email} is {validateEmail(email) ? "Valid" : "Invalid"}
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-
-async function fetchRecords() { return [{id: 1, type: 'record'}]}
-async function fetchAlternateRecords() { return [{ id: 1, type: 'alt-record' }]}
+async function fetchRecords() {
+  return [{ id: 1, type: "record" }];
+}
+async function fetchAlternateRecords() {
+  return [{ id: 1, type: "alt-record" }];
+}
 
 export function SerialLoading() {
+  const [records, setRecords] = useState([]);
+  const [altRecords, setAltRecords] = useState([]);
 
-  const [records, setRecords] = useState([])
-  const [altRecords, setAltRecords] = useState([])
-  
   useEffect(() => {
     async function loadRecords() {
       const recs = await fetchRecords();
@@ -319,42 +340,69 @@ export function SerialLoading() {
     loadRecords();
   }, []);
 
-  return(
+  return (
     <div>
-      {records.map(rec => <div key={rec.id}></div>)}
-      {altRecords.map(rec => <div key={rec.id}></div>)}
+      {records.map((rec) => (
+        <div key={rec.id}></div>
+      ))}
+      {altRecords.map((rec) => (
+        <div key={rec.id}></div>
+      ))}
     </div>
-  )
+  );
 }
 
-async function fetchRecords() { return [{id: 1, type: 'record'}]}
-async function fetchAlternateRecords() { return [{ id: 1, type: 'alt-record' }]}
+async function fetchRecords() {
+  return [{ id: 1, type: "record" }];
+}
+async function fetchAlternateRecords() {
+  return [{ id: 1, type: "alt-record" }];
+}
 
 // Hint: part of the rendering structure is re-rendered frequently unnecessarily
 export function UnoptimizableRenderingStructure(altRecords) {
-  const [records, setRecords] = useState([])
-  
+  const [records, setRecords] = useState([]);
+
   useEffect(() => {
     async function loadRecords() {
       const interval = setInterval(async () => {
         const recs = await fetchRecords();
         setRecords(recs);
       }, 5000);
-  
+
       return () => clearInterval(interval);
     }
     loadRecords();
-  }, []); 
+  }, []);
 
-
-  return(
+  return (
     <div>
       <ul>
-        {records.map(rec => <li key={rec.id}>{rec.id}</li>)}
+        {records.map((rec) => (
+          <li key={rec.id}>{rec.id}</li>
+        ))}
       </ul>
       <ul>
-        {altRecords.map(rec => <li key={rec.id}>{rec.id}</li>)}
+        {altRecords.map((rec) => (
+          <li key={rec.id}>{rec.id}</li>
+        ))}
       </ul>
     </div>
-  )
+  );
+}
+
+function useRenderHook(number) {
+  return <div>{number}</div>;
+}
+
+export function RenderHookComponent() {
+  const [counter, setCounter] = useState();
+  const number = useRenderHook(counter);
+
+  return (
+    <div>
+      <button onClick={() => setCounter((n) => n + 1)}>Click up</button>
+      {number}
+    </div>
+  );
 }
