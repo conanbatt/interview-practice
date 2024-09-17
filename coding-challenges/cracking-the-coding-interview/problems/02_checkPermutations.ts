cc23da22ba6b5abc7dfd81ee930863e8de0b8088
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-09-17 06:10:49 -0300

    remove ref to leetcode

--- coding-challenges/cracking-the-coding-interview/problems/02_checkPermutations.ts
+++ coding-challenges/cracking-the-coding-interview/problems/02_checkPermutations.ts
@@ -1,8 +1,5 @@
 // 2. *Check Permutation*:
 
-// Given two strings, write a method to decide if one is a permutation of the other. 
-// [LeetCode](https://leetcode.com/problems/permutation-in-string/)
+// Given two strings, write a method to decide if one is a permutation of the other.
 
-export default function checkPermutations (s1 : string, s2: string): boolean {
-
-}
+export default function checkPermutations(s1: string, s2: string): boolean {}
