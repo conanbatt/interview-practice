9ef38ea6c0d9836ed76fdbbbdad6afe31dca0275
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-09-22 18:12:19 -0300

    fix tests

--- coding-challenges/cracking-the-coding-interview/problems/__tests__/43_magicIndex.test.ts
+++ coding-challenges/cracking-the-coding-interview/problems/__tests__/43_magicIndex.test.ts
@@ -1,21 +1,28 @@
-import { findMagicIndexDistinct, findMagicIndexNonDistinct } from "../43_magicIndex";
+import {
+  findMagicIndexDistinct,
+  findMagicIndexNonDistinct,
+} from "../43_magicIndex";
 
-describe('magic index', () => {
-    describe('distinct', () => {
-        test('returns correct magic index for distinct input', () => {
-            expect(findMagicIndexDistinct([-2, -1, 0, 2, 4, 6, 8])).toBe(4); // Magic index: 4
+describe("magic index", () => {
+  describe("distinct", () => {
+    test("returns correct magic index for distinct input", () => {
+      expect(findMagicIndexDistinct([-2, -1, 0, 2, 4, 6, 8])).toBe(4); // Magic index: 4
 
-            expect(findMagicIndexDistinct([-3, -2, -1, 4, 5, 7, 9])).toBeNull(); // No magic index
-        });
+      expect(findMagicIndexDistinct([-3, -2, -1, 4, 5, 7, 9])).toBeUndefined(); // No magic index
     });
+  });
 
-    describe('not distinct', () => {
-        test('returns correct magic index for non-distinct input', () => {
-            // Test case where magic index exists
-            expect(findMagicIndexNonDistinct([-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13])).toBe(2); // Magic index: 2
+  describe("not distinct", () => {
+    test("returns correct magic index for non-distinct input", () => {
+      // Test case where magic index exists
+      expect(
+        findMagicIndexNonDistinct([-10, -5, 2, 2, 2, 2, 4, 7, 9, 12, 13]),
+      ).toBe(2); // Magic index: 2
 
-            // Test case where no magic index exists
-            expect(findMagicIndexNonDistinct([-10, -5, 0, 2, 5, 7, 9, 12, 13])).toBeNull(); // No magic index
-        });
+      // Test case where no magic index exists
+      expect(
+        findMagicIndexNonDistinct([-10, -5, 0, 2, 5, 7, 9, 12, 13]),
+      ).toBeUndefined(); // No magic index
     });
+  });
 });
