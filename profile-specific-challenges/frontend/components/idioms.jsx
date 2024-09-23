ae60958ed33b4f0790d69db5a9e028422b9b0d12
Facundo Rebord <farebord@gmail.com>
2024-09-23 15:41:03 -0300

    solution proposal

--- profile-specific-challenges/frontend/components/idioms.jsx
+++ profile-specific-challenges/frontend/components/idioms.jsx
@@ -7,11 +7,12 @@
   Some references used:
   https://claritydev.net/blog/the-most-common-mistakes-when-using-react
 */
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
+// This was missing a return but that showButton could be abstracted out.
 export function FunctionsAsComponents({ buttonText = "Start Now" }) {
   const showButton = () => {
-    <button>{buttonText}</button>;
+    return <button>{buttonText}</button>;
   };
 
   return <div>{showButton()}</div>;
@@ -19,21 +20,29 @@ export function FunctionsAsComponents({ buttonText = "Start Now" }) {
 
 export function objectShallowCopying() {
   const object = { a: { c: 1 }, b: 2 };
-  const copy = { ...object };
+  const copy = JSON.parse(JSON.stringify(object));
   copy.a.c = 2;
   return { ...object };
 }
 
 export function arrayShallowCopying() {
   const array = [{ a: 1 }, { a: 2 }, { a: 3 }];
-  const copy = [...array];
+  const copy = JSON.parse(JSON.stringify(array));
   return copy;
 }
 
+// fetchURL changes forces a new fetch. And the main problem with this one
+// is mostly likely to be caused by parent behavior/changes.
+// Maybe using a ref to compare values instead
 export function UseEffectThrashing({ fetchURL, label }) {
+  const prevValue = useRef(fetchURL);
   useEffect(() => {
+    // If previous value is same as fetchURL, then do nothing.
+    if (prevValue.current === fetchURL) return;
+
     const fetchData = async () => {
       await fetch(fetchURL);
+      prevValue.current = fetchURL;
     };
 
     fetchData();
@@ -48,7 +57,7 @@ export function UseEffectThrashing({ fetchURL, label }) {
 
 export function UseEffectDerivedCalculation() {
   const [remainder, setReminder] = useState();
-  const [clickedTimes, setClickedTimes] = useState();
+  const [clickedTimes, setClickedTimes] = useState(0);
 
   useEffect(() => {
     setReminder(clickedTimes % 5);
@@ -59,25 +68,26 @@ export function UseEffectDerivedCalculation() {
   return (
     <div>
       <button onClick={handleClick}>Add Click Count</button>
-      <span>{sum}</span>
+      <span>{clickedTimes}</span>
       <span>{remainder}</span>
     </div>
   );
 }
 
 export function UseStateDerivedCalculation() {
-  const [remainder, setReminder] = useState();
-  const [clickedTimes, setClickedTimes] = useState();
+  const [remainder, setReminder] = useState(0);
+  const [clickedTimes, setClickedTimes] = useState(0);
 
   const handleClick = () => {
+    // State updates don't occur inmmediately
     setClickedTimes(clickedTimes + 1);
-    setReminder(clickedTimes % 5);
+    setReminder((clickedTimes + 1) % 5);
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add Click Count</button>
-      <span>{sum}</span>
+      <span>{clickedTimes}</span>
       <span>{remainder}</span>
     </div>
   );
@@ -87,22 +97,29 @@ export function DirtyUnmount() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const int = setInterval(() => {
       setTime((t) => t + 1);
     }, 1000);
+
+    return () => {
+      clearInterval(int);
+    };
   }, []);
 
   return <div>Clock in seconds: {time}</div>;
 }
 
 export function AvoidingUseState() {
-  const ref = useRef("Unmounted");
+  const [mounted, setMounted] = useState("Unmounted");
 
   useEffect(() => {
-    ref.current = "Mounted";
+    setMounted("Mounted");
+    return () => {
+      setMounted("Unmounted");
+    };
   }, []);
 
-  return <div>{ref.current}</div>;
+  return <div>{mounted}</div>;
 }
 
 async function API() {
@@ -111,13 +128,13 @@ async function API() {
 
 export function UnrenderableState() {
   const [result, setResult] = useState();
-  let loading = false;
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      loading = true;
+      setLoading(true);
       const result = await API();
-      loading = false;
+      setLoading(false);
       setResult(result);
     };
 
@@ -126,17 +143,19 @@ export function UnrenderableState() {
 
   return (
     <div>
-      <span>Loading: {loading}</span>
-      Result:{result}
+      {loading && <span>Loading...</span>}
+      {result && <>Result: {result}</>}
     </div>
   );
 }
 
+const calendarDays = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30,
+];
+
+// I'm assuming the issue is the cause of re-declarations of the calendar days array.
 export function CrudeDeclarations() {
-  const calendarDays = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30,
-  ];
   return (
     <ol>
       {calendarDays.map((val) => (
@@ -146,9 +165,10 @@ export function CrudeDeclarations() {
   );
 }
 
+// hmmm
 export function MagicNumbers(age) {
   return (
-    <ol>{age < 18 ? <div>Spicy</div> : <div>You are not old enough</div>}</ol>
+    <ol>{age >= 18 ? <li>Spicy</li> : <li>You are not old enough</li>}</ol>
   );
 }
 
@@ -158,7 +178,12 @@ export function UnidiomaticHTMLStructure() {
 
   return (
     <div>
-      <input value={name} name="name" type="text" onChange={setName} />
+      <input
+        value={name}
+        name="name"
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      />
       <button type="submit" onClick={handleSubmit}>
         Submit
       </button>
@@ -177,20 +202,35 @@ export function CrudeStateManagement() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input value={name} name="name" type="text" onChange={setName} />
-      <input value={age} name="age" type="number" onChange={setAge} />
+      <input
+        value={name}
+        name="name"
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        value={age}
+        name="age"
+        type="number"
+        onChange={(e) => setAge(e.target.value)}
+      />
       <input
         value={location}
         name="location"
         type="text"
-        onChange={setLocation}
+        onChange={(e) => setLocation(e.target.value)}
       />
-      <input value={email} name="email" type="email" onChange={setEmail} />
+      <input
+        value={email}
+        name="email"
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <input
         value={password}
         name="password"
         type="password"
-        onChange={setPassword}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <button type="submit">Submit</button>
     </form>
@@ -202,26 +242,36 @@ export function UnidiomaticHTMLHierarchy() {
   const asks = [1, 2, 3];
 
   return (
-    <li>
+    <div>
       {bids.map((bid, i) => (
         <span key={i}>{bid}</span>
       ))}
       {asks.map((ask, j) => (
         <span key={j + "asks"}>{ask}</span>
       ))}
-    </li>
+    </div>
   );
 }
 
 export function SubstandardDataStructure() {
-  const [error, setError] = useState("");
+  const [errors, setErrors] = useState([]);
 
   return (
     <div>
-      <button onClick={() => setError("Error A")}>Throw Error A</button>
-      <button onClick={() => setError("Error B")}>Throw Error B</button>
-      <button onClick={() => setError("")}>Clear Errors</button>
-      <div>{error}</div>
+      <button onClick={() => setErrors([...errors, "Error A"])}>
+        Throw Error A
+      </button>
+      <button onClick={() => setErrors([...errors, "Error B"])}>
+        Throw Error B
+      </button>
+      <button onClick={() => setErrors([])}>Clear Errors</button>
+      <div>
+        <ul>
+          {errors.map((error) => (
+            <li>{error}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
@@ -231,19 +281,19 @@ export function DangerousIdentifier() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const person = new FormData(e.target);
-    setPeople((ppl) => [...ppl, ...person]);
+    const formData = new FormData(e.target);
+    setPeople((ppl) => [...ppl, { name: formData.get("name") }]);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input name="name" type="text" />
         <button>Add Person</button>
       </form>
       <ul>
-        {people.map((person) => (
-          <span key={person.name}>{person.name}</span>
+        {people.map((person, id) => (
+          <span key={`${id}-${person.name}`}>{person.name}</span>
         ))}
       </ul>
     </div>
@@ -272,10 +322,13 @@ export function UnnecessaryEffectTriggering() {
   useEffect(() => {
     async function enhanceRecord() {
       const enriched = await fetchDetails(leader);
-      setLeader(enriched);
+      // Not sure if this check if what is required or also another comparison on the first useEffect
+      if (JSON.stringify(enriched) !== JSON.stringify(leader)) {
+        setLeader(enriched);
+      }
     }
     enhanceRecord();
-  }, [leader]);
+  }, [leader]); // My second attempt was to JSON.stringify the dependency
 
   return (
     <div>
@@ -293,7 +346,7 @@ async function trackClick(ids) {
 export function IncorrectDependencies(records) {
   const handleClick = useCallback(() => {
     trackClick(records);
-  }, [records]);
+  }, [JSON.stringify(records)]); // I wonder if this is what's needed here and above.
 
   return (
     <div>
@@ -306,7 +359,7 @@ export function IncorrectDependencies(records) {
 }
 
 export function UnnecessaryFunctionRedefinitions(emails) {
-  const validateEmail = (email) => email.includes("@");
+  const validateEmail = useCallback((email) => email.includes("@"), []); // adding a callback avoid redefinitions
 
   return (
     <div>
@@ -332,8 +385,10 @@ export function SerialLoading() {
 
   useEffect(() => {
     async function loadRecords() {
-      const recs = await fetchRecords();
-      const altRecs = await fetchAlternateRecords();
+      const [recs, altRecs] = await Promise.all([
+        fetchRecords(),
+        fetchAlternateRecords(),
+      ]);
       setRecords(recs);
       setAltRecords(altRecs);
     }
@@ -343,7 +398,7 @@ export function SerialLoading() {
   return (
     <div>
       {records.map((rec) => (
-        <div key={rec.id}></div>
+        <div key={rec.id}></div> // Also this doesn't renders nothing, but I guess its the parallel fetching behavior whats required here.
       ))}
       {altRecords.map((rec) => (
         <div key={rec.id}></div>
@@ -367,7 +422,10 @@ export function UnoptimizableRenderingStructure(altRecords) {
     async function loadRecords() {
       const interval = setInterval(async () => {
         const recs = await fetchRecords();
-        setRecords(recs);
+        // Same as above
+        if (JSON.stringify(recs) !== JSON.stringify(records)) {
+          setRecords(recs);
+        }
       }, 5000);
 
       return () => clearInterval(interval);
@@ -393,33 +451,49 @@ export function UnoptimizableRenderingStructure(altRecords) {
 
 // RenderHooks
 
+// hooks shouldn't return JSX, this can lead to confussion
+// hooks are used to abstract logic.
+// this needs to be a component
 function useRenderHook(number) {
   return <div>{number}</div>;
 }
 
+const NumberCount = ({ number }) => {
+  return <div>{number}</div>;
+};
+
 export function RenderHookComponent() {
   const [counter, setCounter] = useState();
-  const number = useRenderHook(counter);
 
   return (
     <div>
       <button onClick={() => setCounter((n) => n + 1)}>Click up</button>
-      {number}
+      <NumberCount number={count} />
     </div>
   );
 }
 
 // Prop Drilling
 
-function Child3({ counter } = { counter: number }) {
+// There are many alternative ways to solve this,
+// but for this case using ContextAPI is enough
+const CounterContext = createContext(5);
+
+function Child3() {
   <div>{counter}</div>;
 }
-function Child2({ counter } = { counter: number }) {
-  <Child3 counter={counter} />;
+function Child2() {
+  <Child3 />;
 }
-function Child({ counter } = { counter: number }) {
-  <Child2 counter={counter} />;
+function Child() {
+  <Child2 />;
 }
+// There are many alternative ways to solve this,
+// but for this case using ContextAPI is enough
 function ExcessivePropDrilling() {
-  return <Child counter={5} />;
+  return (
+    <CounterContext.Provider>
+      <Child />
+    </CounterContext.Provider>
+  );
 }
