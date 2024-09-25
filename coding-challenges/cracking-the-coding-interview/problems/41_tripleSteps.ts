1898dc7fe23404cb698d7cef7db5d5d974026820
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-09-25 15:07:22 -0300

    various fixes

--- coding-challenges/cracking-the-coding-interview/problems/41_tripleSteps.ts
+++ coding-challenges/cracking-the-coding-interview/problems/41_tripleSteps.ts
@@ -1,9 +1,8 @@
-// 1. *Triple Step*: 
+// 1. *Triple Step*:
 
-// A child is running up a staircase with n steps and can hop either 
-// 1 step, 2 steps, or 3 steps at a time. Implement a method to count 
+// A child is running up a staircase with n steps and can hop either
+// 1 step, 2 steps, or 3 steps at a time. Implement a method to count
 // how many possible ways the child can run up the stairs.
+//
 
-export default function tripleStep(n: number): number {
-   
-}
+export default function tripleStep(n: number): number {}
--- coding-challenges/cracking-the-coding-interview/problems/42_robotInAGrid.ts
+++ coding-challenges/cracking-the-coding-interview/problems/42_robotInAGrid.ts
@@ -1,13 +1,11 @@
-// 2. *Robot in a Grid*: 
+// 2. *Robot in a Grid*:
 
-// Imagine a robot sitting on the upper left corner of a grid with r rows and c columns. 
-// The robot can only move in two directions, right and down, but certain cells are 
+// Imagine a robot sitting on the upper left corner of a grid with r rows and c columns.
+// The robot can only move in two directions, right and down, but certain cells are
 // "off limits" such that the robot cannot step on them.
 // Design an algorithm to find a path for the robot from the top left to the bottom right.
 
-
 type Grid = boolean[][];
+type Path = Array<[number, number]>;
 
-export default function robotInAGrid(grid: Grid): [number, number][] | null {
-   
-}
+export default function robotInAGrid(grid: Grid): [number, number][] | false {}
