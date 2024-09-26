73dbadd7be11391d273d110cd5bf7beb0f01bf48
Facundo Rebord <farebord@gmail.com>
2024-09-26 14:24:13 -0300

    signup challenge #1

--- profile-specific-challenges/frontend/pages/challenges/signup.js
+++ profile-specific-challenges/frontend/pages/challenges/signup.js
@@ -1,34 +1,125 @@
+import { useState } from "react";
+
 function API(data) {
   return new Promise((res) => {
-    setTimeout(
-      () =>
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
         res({
           success: "Your Account has been successfully created!",
-          error: "Username is taken",
-        }),
-      1000
-    );
+        });
+      } else {
+        res({ error: "Username is taken" });
+      }
+    }, 1000);
   });
 }
 
-export default function SignupForm() {
-  return(
-    <>
-      <h1>
-        Signup Form
-      </h1>
-      <p>
-        Build a user Signup form in React with the following features.
+const validateEmail = (email) => {
+  if (!email.match(/^[^@]+@[^@]+\.[^@]+$/))
+    return ["Email is not a valid email address"];
+  return [];
+};
 
-        1. An email and a password input
-        2. Email must have an “@” and the domain side must include a “.”
-        3. Password must include
-            1.  at least one special character
-            2. one number and be at least 8 characters
-        4. Submission request handling  
-           1. Utilze the mock API function to handle submissions
-        5. Basic aesthetics with pure CSS
-      </p>
+const validatePassword = (password) => {
+  const errors = [];
+  if (!password.match(/\d/))
+    errors.push("The password must contain at least one digit");
+  if (!password.match(/\W/))
+    errors.push("The password must contain at least one special letter"); // This doesn't includes underscores
+  if (password.length < 8)
+    errors.push("The password must be at least 8 characters");
+  return errors;
+};
+
+const submitSignupForm = async ({ email, password }) => {
+  const errors = [...validateEmail(email), ...validatePassword(password)];
+
+  if (errors.length > 0) {
+    return {
+      data: null,
+      errors,
+    };
+  }
+
+  const request = await API({ email, password });
+
+  if (request.error) {
+    return {
+      data: null,
+      errors: [request.error],
+    };
+  }
+
+  return {
+    data: request.success,
+    errors: null,
+  };
+};
+
+export default function SignupForm() {
+  const [formState, setFormState] = useState({});
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setErrors([]);
+    setLoading(true);
+    const { data, errors } = await submitSignupForm(formState);
+
+    setLoading(false);
+
+    if (errors) {
+      setErrors(errors);
+      return;
+    }
+
+    alert(data);
+  };
+
+  return (
+    <>
+      <h1>Signup Form</h1>
+      <form onSubmit={handleSubmit}>
+        {errors && (
+          <div>
+            <ul>
+              {errors.map((error, i) => (
+                <li key={`error-${i}`}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="Enter your email address"
+            value={formState.email ?? ""}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Password:</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={formState.password ?? ""}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div>
+          <button disabled={loading} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
